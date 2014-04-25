3269fae7ecbf733b1948e4a5f7bb3e2eaab4eba1
Jeffery Petit <petit.jeffery@gmail.com>
2014-04-25 09:19:34 +0200

    Implémentation des menus

--- Appli/src/com/ensaitechnomobile/meteolocale/MeteoPrincipal.java
+++ Appli/src/com/ensaitechnomobile/meteolocale/MeteoPrincipal.java
@@ -12,11 +12,16 @@ import java.net.URL;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.ensaitechnomobile.menuprincipal.MenuPrincipal;
 import com.example.pamplemousse.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MeteoPrincipal extends Activity {
 	
@@ -96,6 +101,50 @@ public class MeteoPrincipal extends Activity {
 			contenu += ligne;
 		}
 		return contenu;
+	}
+	
+	// Implémentation du menu
+
+	// Méthode qui se déclenchera lorsque vous appuierez sur le bouton menu du
+	// téléphone
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Création d'un MenuInflater qui va permettre d'instancier un Menu XML
+		// en un objet Menu
+		MenuInflater inflater = getMenuInflater();
+		// Instanciation du menu XML spécifier en un objet Menu
+		inflater.inflate(R.layout.menu, menu);
+
+		// Il n'est pas possible de modifier l'icône d'entête du sous-menu via
+		// le fichier XML on le fait donc en JAVA
+		// menu.getItem(0).getSubMenu().setHeaderIcon(R.drawable.option_white);
+
+		return true;
+	}
+
+	// Méthode qui se déclenchera au clic sur un item
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// On regarde quel item a été cliqué grâce à son id et on déclenche une
+		// action
+		switch (item.getItemId()) {
+		case R.id.option:
+			Toast.makeText(this, "Option", Toast.LENGTH_SHORT)
+					.show();
+			return true;
+		case R.id.favoris:
+			Toast.makeText(this, "Favoris", Toast.LENGTH_SHORT)
+					.show();
+			return true;
+		case R.id.stats:
+			Toast.makeText(this, "Stats", Toast.LENGTH_SHORT)
+					.show();
+			return true;
+		case R.id.quitter:
+			// Pour fermer l'application il suffit de faire finish()
+			finish();
+			return true;
+		}
+		return false;
 	}
 	
 }
