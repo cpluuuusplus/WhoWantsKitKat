4504005540a953dd83b6d9c8607b9fc394ca0902
Nicolas Edwards <nicolas.ae@free.fr>
2014-04-25 17:57:15 +0200

    Rajout d'un élément (en commentaire) pour (espérons) auto-remplir les webview

--- Appli/src/com/ensaitechnomobile/pamplemousse/WebViewMail.java
+++ Appli/src/com/ensaitechnomobile/pamplemousse/WebViewMail.java
@@ -11,5 +11,21 @@ public class WebViewMail extends Activity {
 		WebView webview = new WebView(this);
 		setContentView(webview);
 		webview.loadUrl("https://sso-cas.ensai.fr/cas/login?service=http://ent2.ensai.fr/Login");
+		
+//		TODO intégrer ceci pour (espérons) saisir le mot de passe automatiquement
+//		webView.getSettings().setJavaScriptEnabled(true);   
+//
+//	    webView.loadUrl("http://your.url");
+//
+//	    webView.setWebViewClient(new WebViewClient() {
+//
+//	        public void onPageFinished(WebView view, String url) {
+//	            view.loadUrl("javascript:document.getElementsByName('school')[0].value = 'schoolname'");
+//	            view.loadUrl("javascript:document.getElementsByName('j_username')[0].value = 'username'");
+//	            view.loadUrl("javascript:document.getElementsByName('j_password')[0].value = 'password'");
+//
+//	            view.loadUrl("javascript:document.forms['login'].submit()");
+//	        }
+//	    });
 	}
 }
