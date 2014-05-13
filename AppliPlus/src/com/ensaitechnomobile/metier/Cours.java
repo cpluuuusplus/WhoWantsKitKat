05156f0b3bad5556e8b3bd121462e3604425fe93
Jeffery Petit <petit.jeffery@gmail.com>
2014-05-13 12:38:51 +0200

    Changements Soutenance et renommage de l'application

---
+++ AppliPlus/src/com/ensaitechnomobile/metier/Cours.java
@@ -0,0 +1,52 @@
+package com.ensaitechnomobile.metier;
+
+public class Cours implements Comparable<Cours> {
+
+	/* Attributs */
+	private long debut, fin;
+	private String nom, salle, uid;
+
+	/* Constructeur */
+	public Cours(long debut, long fin, String nom, String salle, String uid) {
+		super();
+		this.debut = debut;
+		this.fin = fin;
+		this.nom = nom;
+		this.salle = salle;
+		this.uid = uid;
+	}
+
+	/* Accesseurs et mutateurs */
+	public long getDebut() {
+		return debut;
+	}
+
+	public long getFin() {
+		return fin;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getSalle() {
+		return salle;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return ("debut: " + this.debut + ", nom: " + this.nom + ", salle: "
+				+ this.salle + " fin: " + this.fin);
+	}
+
+	@Override
+	public int compareTo(Cours another) {
+		return (int) (this.debut - another.debut);
+	}
+
+}
